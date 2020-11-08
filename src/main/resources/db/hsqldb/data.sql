5d0a504bf400d117f7cad23d5ddbe77f1590913b
juanp <juapormon@alum.us.es>
2020-11-08 15:52:05 +0100

    Implement score, first attempt.

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -18,6 +18,9 @@ INSERT INTO vets VALUES (4, 'Rafael', 'Ortega');
 INSERT INTO vets VALUES (5, 'Henry', 'Stevens');
 INSERT INTO vets VALUES (6, 'Sharon', 'Jenkins');
 
+INSERT INTO scores VALUES (1, 7, 'esta es una puntuacion');
+INSERT INTO scores VALUES (2, 4, 'esta es otra puntuacion');
+
 INSERT INTO specialties VALUES (1, 'radiology');
 INSERT INTO specialties VALUES (2, 'surgery');
 INSERT INTO specialties VALUES (3, 'dentistry');
@@ -66,5 +69,6 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'snoop dog', '
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
-INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04', 'spayed');
+INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04', 'spayed');´
+
 
