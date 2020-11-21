d5c6f93090728e04f847e99bafe7e8ee6b86381a
Luis <luiparlop1@alum.us.es>
2020-11-21 19:47:18 +0100

    Añadidos atributos a teacher

--- src/main/java/org/springframework/samples/petclinic/model/Teacher.java
+++ src/main/java/org/springframework/samples/petclinic/model/Teacher.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.model;
 
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -16,8 +17,22 @@ public class Teacher extends Person{
 	@OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "username", referencedColumnName = "username")
 
+	@Column(name = "user")
 	private User user;
 	
+	@Column(name = "college")
+	private College college;
+	
+	@Column(name = "personal_experience")
+	private PersonalExperience personalExperience;
+	
+	@Column(name = "professional_experience")
+	private ProfessionalExperience professionalExperience;
+
+	
+	@Column(name = "research_experience")
+	private ResearchExperience researchExperience;
+
 	
 	public User getUser() {
 		return user;
@@ -27,7 +42,36 @@ public class Teacher extends Person{
 		this.user = user;
 	}
 	
+	public College getCollege() {
+		return college;
+	}
+	
+	public void setCollege(College college) {
+		this.college = college;
+	}
+	
+	public PersonalExperience getPersonalExperience() {
+		return personalExperience;
+	}
 
+	public void setPersonalExperience(PersonalExperience personalExperience) {
+		this.personalExperience = personalExperience;
+	}
+	
+	public ProfessionalExperience getProfessionalExperience() {
+		return professionalExperience;
+	}
 
+	public void setProfessionalExperience(ProfessionalExperience professionalExperience) {
+		this.professionalExperience = professionalExperience;
+	}
+	
+	public ResearchExperience getResearchExperience() {
+		return researchExperience;
+	}
+
+	public void setResearchExperience(ResearchExperience researchExperience) {
+		this.researchExperience = researchExperience;
+	}
 
 }
