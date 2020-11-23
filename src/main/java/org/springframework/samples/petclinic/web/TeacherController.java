e16628e41fba2693fc2523a7b6f8a368365c9677
juanp <juapormon@alum.us.es>
2020-11-23 12:54:43 +0100

    Teacher details implemented

--- src/main/java/org/springframework/samples/petclinic/web/TeacherController.java
+++ src/main/java/org/springframework/samples/petclinic/web/TeacherController.java
@@ -5,13 +5,17 @@ package org.springframework.samples.petclinic.web;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Teacher;
 import org.springframework.samples.petclinic.model.Teachers;
 import org.springframework.samples.petclinic.service.TeacherService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TeacherController {
@@ -50,6 +54,14 @@ public class TeacherController {
 		return teachers;
 	}
 	
+	
+	@GetMapping(value = "/teachers/{teacherId}")
+	public ModelAndView showTeacher(@PathVariable("teacherId") int teacherId) {
+		ModelAndView mav = new ModelAndView("teachers/teacherDetails");
+		mav.addObject(this.teacherService.findTeacherById(teacherId));
+		return mav;
+	}
+
 	
 }
 
