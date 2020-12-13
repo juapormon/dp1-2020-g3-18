f27dcbeeb340bede5e1e46fbc88eaaf45fd99248
Luis <luiparlop1@alum.us.es>
2020-12-13 21:41:33 +0100

    revirtiendo errores

--- src/main/java/org/springframework/samples/petclinic/web/TeacherController.java
+++ src/main/java/org/springframework/samples/petclinic/web/TeacherController.java
@@ -172,17 +172,6 @@ public class TeacherController {
 		}
 	}
 
-	
-	@GetMapping(value = { "/teachers/teachersByDepartment/{departmentName}" })
-	public String showTeachersByDepartment(@PathVariable("departmentName") String departmentName, Map<String, Object> model) {
-		
-		Teachers teachers = new Teachers();
-		teachers.getTeachersList().addAll(this.teacherService.findTeachersByDepartment(departmentName));
-		model.put("teachers", teachers);
-		return "teachers/teachersByDepartment/{departmentName}";
-		
-	}
-
 	@GetMapping(value = "teachers/{teacherId}/scores/{scoreId}/edit")
 	public String initEditForm(@PathVariable int teacherId, @PathVariable int scoreId, ModelMap model) {
 		Score score = this.scoreService.findScoreById(scoreId);
