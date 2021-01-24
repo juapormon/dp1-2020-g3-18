e03ba076d32692f3772c474464857599cc74c85e
josrojrom1-20 <josrojrom1@alum.us.es>
2021-01-24 15:52:50 +0100

    Añadidos tests validadores en teacher y score

--- src/test/java/rateacher/tests/validators/TeacherValidatorTest.java
+++ src/test/java/rateacher/tests/validators/TeacherValidatorTest.java
@@ -32,17 +32,39 @@ public class TeacherValidatorTest {
 
 	@Test
 	void ShouldNotValidateWhenFirstNameEmpty() {
+		System.out.println("---------------------------------------------");
+		System.out.println("Should not validate when first name empty");
+		System.out.println("---------------------------------------------");
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
-	
+
 		Teacher teacher = new Teacher();
 		teacher.setFirstName("");
 		teacher.setLastName("García");
-		
+
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Teacher>> constraintViolations = validator.validate(teacher);
-		assertTrue(constraintViolations.size()==1);
+		assertTrue(constraintViolations.size() == 1);
 		ConstraintViolation<Teacher> violation = constraintViolations.iterator().next();
 		assertTrue(violation.getPropertyPath().toString().equals("firstName"));
+		assertTrue(violation.getMessage().equals("must not be empty"));
+	}
+	
+	@Test
+	void ShouldNotValidateWhenLastNameEmpty() {
+		System.out.println("---------------------------------------------");
+		System.out.println("Should not validate when last name empty");
+		System.out.println("---------------------------------------------");
+		LocaleContextHolder.setLocale(Locale.ENGLISH);
+
+		Teacher teacher = new Teacher();
+		teacher.setFirstName("Lucía");
+		teacher.setLastName("");
+
+		Validator validator = createValidator();
+		Set<ConstraintViolation<Teacher>> constraintViolations = validator.validate(teacher);
+		assertTrue(constraintViolations.size() == 1);
+		ConstraintViolation<Teacher> violation = constraintViolations.iterator().next();
+		assertTrue(violation.getPropertyPath().toString().equals("lastName"));
 		assertTrue(violation.getMessage().equals("must not be empty"));
 	}
 
